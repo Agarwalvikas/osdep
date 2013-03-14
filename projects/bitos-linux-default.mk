399725e49469993246edc0681166c532bf35adc9
Embedthis Software <dev@embedthis.com>
2013-03-14 13:37:09 -0700

    Gen projects

---
+++ projects/bitos-linux-default.mk
@@ -0,0 +1,126 @@
+#
+#   bitos-linux-default.mk -- Makefile to build Bit O/S Layer for linux
+#
+
+PRODUCT           := bitos
+VERSION           := 0.8.1
+BUILD_NUMBER      := 0
+PROFILE           := default
+ARCH              := $(shell uname -m | sed 's/i.86/x86/;s/x86_64/x64/;s/arm.*/arm/;s/mips.*/mips/')
+OS                := linux
+CC                := /usr/bin/gcc
+LD                := /usr/bin/ld
+CONFIG            := $(OS)-$(ARCH)-$(PROFILE)
+LBIN              := $(CONFIG)/bin
+
+
+CFLAGS            += -fPIC   -w
+DFLAGS            += -D_REENTRANT -DPIC  $(patsubst %,-D%,$(filter BIT_%,$(MAKEFLAGS))) 
+IFLAGS            += -I$(CONFIG)/inc
+LDFLAGS           += '-Wl,--enable-new-dtags' '-Wl,-rpath,$$ORIGIN/' '-rdynamic'
+LIBPATHS          += -L$(CONFIG)/bin
+LIBS              += -lpthread -lm -lrt -ldl
+
+DEBUG             := debug
+CFLAGS-debug      := -g
+DFLAGS-debug      := -DBIT_DEBUG
+LDFLAGS-debug     := -g
+DFLAGS-release    := 
+CFLAGS-release    := -O2
+LDFLAGS-release   := 
+CFLAGS            += $(CFLAGS-$(DEBUG))
+DFLAGS            += $(DFLAGS-$(DEBUG))
+LDFLAGS           += $(LDFLAGS-$(DEBUG))
+
+BIT_ROOT_PREFIX   := 
+BIT_BASE_PREFIX   := $(BIT_ROOT_PREFIX)/usr/local
+BIT_DATA_PREFIX   := $(BIT_ROOT_PREFIX)/
+BIT_STATE_PREFIX  := $(BIT_ROOT_PREFIX)/var
+BIT_APP_PREFIX    := $(BIT_BASE_PREFIX)/lib/$(PRODUCT)
+BIT_VAPP_PREFIX   := $(BIT_APP_PREFIX)/$(VERSION)
+BIT_BIN_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local/bin
+BIT_INC_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local/include
+BIT_LIB_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local/lib
+BIT_MAN_PREFIX    := $(BIT_ROOT_PREFIX)/usr/local/share/man
+BIT_SBIN_PREFIX   := $(BIT_ROOT_PREFIX)/usr/local/sbin
+BIT_ETC_PREFIX    := $(BIT_ROOT_PREFIX)/etc/$(PRODUCT)
+BIT_WEB_PREFIX    := $(BIT_ROOT_PREFIX)/var/www/$(PRODUCT)-default
+BIT_LOG_PREFIX    := $(BIT_ROOT_PREFIX)/var/log/$(PRODUCT)
+BIT_SPOOL_PREFIX  := $(BIT_ROOT_PREFIX)/var/spool/$(PRODUCT)
+BIT_CACHE_PREFIX  := $(BIT_ROOT_PREFIX)/var/spool/$(PRODUCT)/cache
+BIT_SRC_PREFIX    := $(BIT_ROOT_PREFIX)$(PRODUCT)-$(VERSION)
+
+
+
+unexport CDPATH
+
+ifndef SHOW
+.SILENT:
+endif
+
+all build compile: prep $(TARGETS)
+
+.PHONY: prep
+
+prep:
+	@echo "      [Info] Use "make SHOW=1" to trace executed commands."
+	@if [ "$(CONFIG)" = "" ] ; then echo WARNING: CONFIG not set ; exit 255 ; fi
+	@if [ "$(BIT_APP_PREFIX)" = "" ] ; then echo WARNING: BIT_APP_PREFIX not set ; exit 255 ; fi
+	@[ ! -x $(CONFIG)/bin ] && mkdir -p $(CONFIG)/bin; true
+	@[ ! -x $(CONFIG)/inc ] && mkdir -p $(CONFIG)/inc; true
+	@[ ! -x $(CONFIG)/obj ] && mkdir -p $(CONFIG)/obj; true
+	@[ ! -f $(CONFIG)/inc/bit.h ] && cp projects/bitos-linux-default-bit.h $(CONFIG)/inc/bit.h ; true
+	@[ ! -f $(CONFIG)/inc/bitos.h ] && cp src/bitos.h $(CONFIG)/inc/bitos.h ; true
+	@if ! diff $(CONFIG)/inc/bit.h projects/bitos-linux-default-bit.h >/dev/null ; then\
+		echo cp projects/bitos-linux-default-bit.h $(CONFIG)/inc/bit.h  ; \
+		cp projects/bitos-linux-default-bit.h $(CONFIG)/inc/bit.h  ; \
+	fi; true
+
+clean:
+
+clobber: clean
+	rm -fr ./$(CONFIG)
+
+
+
+#
+#   version
+#
+version: $(DEPS_1)
+	@echo NN 0.8.1-0
+
+#
+#   stop
+#
+stop: $(DEPS_2)
+
+#
+#   installBinary
+#
+DEPS_3 += stop
+
+installBinary: $(DEPS_3)
+
+#
+#   start
+#
+start: $(DEPS_4)
+
+#
+#   install
+#
+DEPS_5 += stop
+DEPS_5 += installBinary
+DEPS_5 += start
+
+install: $(DEPS_5)
+	
+
+#
+#   uninstall
+#
+DEPS_6 += stop
+
+uninstall: $(DEPS_6)
+	
+
---
+++ projects/bitos-vxworks-default.mk
@@ -0,0 +1,130 @@
+#
+#   bitos-vxworks-default.mk -- Makefile to build Bit O/S Layer for vxworks
+#
+
+export WIND_BASE := $(WIND_BASE)
+export WIND_HOME := $(WIND_BASE)/..
+export WIND_PLATFORM := $(WIND_PLATFORM)
+
+PRODUCT           := bitos
+VERSION           := 0.8.1
+BUILD_NUMBER      := 0
+PROFILE           := default
+ARCH              := $(shell uname -m | sed 's/i.86/x86/;s/x86_64/x64/;s/arm.*/arm/;s/mips.*/mips/')
+OS                := vxworks
+CC                := ccpentium
+LD                := /usr/bin/ld
+CONFIG            := $(OS)-$(ARCH)-$(PROFILE)
+LBIN              := $(CONFIG)/bin
+
+
+CFLAGS            += -fno-builtin -fno-defer-pop -fvolatile  -w
+DFLAGS            += -D_REENTRANT -DVXWORKS -DRW_MULTI_THREAD -D_GNU_TOOL  -DCPU=PENTIUM $(patsubst %,-D%,$(filter BIT_%,$(MAKEFLAGS))) 
+IFLAGS            += -I$(CONFIG)/inc -I$(WIND_BASE)/target/h -I$(WIND_BASE)/target/h/wrn/coreip
+LDFLAGS           += '-Wl,-r'
+LIBPATHS          += -L$(CONFIG)/bin -L$(CONFIG)/bin
+LIBS              += 
+
+DEBUG             := debug
+CFLAGS-debug      := -g
+DFLAGS-debug      := -DBIT_DEBUG
+LDFLAGS-debug     := -g
+DFLAGS-release    := 
+CFLAGS-release    := -O2
+LDFLAGS-release   := 
+CFLAGS            += $(CFLAGS-$(DEBUG))
+DFLAGS            += $(DFLAGS-$(DEBUG))
+LDFLAGS           += $(LDFLAGS-$(DEBUG))
+
+BIT_ROOT_PREFIX   := deploy
+BIT_BASE_PREFIX   := $(BIT_ROOT_PREFIX)
+BIT_DATA_PREFIX   := $(BIT_VAPP_PREFIX)
+BIT_STATE_PREFIX  := $(BIT_VAPP_PREFIX)
+BIT_BIN_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_INC_PREFIX    := $(BIT_VAPP_PREFIX)/inc
+BIT_LIB_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_MAN_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_SBIN_PREFIX   := $(BIT_VAPP_PREFIX)
+BIT_ETC_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_WEB_PREFIX    := $(BIT_VAPP_PREFIX)/web
+BIT_LOG_PREFIX    := $(BIT_VAPP_PREFIX)
+BIT_SPOOL_PREFIX  := $(BIT_VAPP_PREFIX)
+BIT_CACHE_PREFIX  := $(BIT_VAPP_PREFIX)
+BIT_APP_PREFIX    := $(BIT_BASE_PREFIX)
+BIT_VAPP_PREFIX   := $(BIT_APP_PREFIX)
+BIT_SRC_PREFIX    := $(BIT_ROOT_PREFIX)/usr/src/$(PRODUCT)-$(VERSION)
+
+
+
+unexport CDPATH
+
+ifndef SHOW
+.SILENT:
+endif
+
+all build compile: prep $(TARGETS)
+
+.PHONY: prep
+
+prep:
+	@echo "      [Info] Use "make SHOW=1" to trace executed commands."
+	@if [ "$(CONFIG)" = "" ] ; then echo WARNING: CONFIG not set ; exit 255 ; fi
+	@if [ "$(BIT_APP_PREFIX)" = "" ] ; then echo WARNING: BIT_APP_PREFIX not set ; exit 255 ; fi
+	@[ ! -x $(CONFIG)/bin ] && mkdir -p $(CONFIG)/bin; true
+	@[ ! -x $(CONFIG)/inc ] && mkdir -p $(CONFIG)/inc; true
+	@[ ! -x $(CONFIG)/obj ] && mkdir -p $(CONFIG)/obj; true
+	@[ ! -f $(CONFIG)/inc/bit.h ] && cp projects/bitos-vxworks-default-bit.h $(CONFIG)/inc/bit.h ; true
+	@[ ! -f $(CONFIG)/inc/bitos.h ] && cp src/bitos.h $(CONFIG)/inc/bitos.h ; true
+	@if ! diff $(CONFIG)/inc/bit.h projects/bitos-vxworks-default-bit.h >/dev/null ; then\
+		echo cp projects/bitos-vxworks-default-bit.h $(CONFIG)/inc/bit.h  ; \
+		cp projects/bitos-vxworks-default-bit.h $(CONFIG)/inc/bit.h  ; \
+	fi; true
+
+clean:
+
+clobber: clean
+	rm -fr ./$(CONFIG)
+
+
+
+#
+#   version
+#
+version: $(DEPS_1)
+	@echo NN 0.8.1-0
+
+#
+#   stop
+#
+stop: $(DEPS_2)
+
+#
+#   installBinary
+#
+DEPS_3 += stop
+
+installBinary: $(DEPS_3)
+
+#
+#   start
+#
+start: $(DEPS_4)
+
+#
+#   install
+#
+DEPS_5 += stop
+DEPS_5 += installBinary
+DEPS_5 += start
+
+install: $(DEPS_5)
+	
+
+#
+#   uninstall
+#
+DEPS_6 += stop
+
+uninstall: $(DEPS_6)
+	
+

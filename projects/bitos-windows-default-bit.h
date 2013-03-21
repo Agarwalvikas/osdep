6df11820c18363d819662b291bfcbc80e49b2441
Embedthis Software <dev@embedthis.com>
2013-03-21 14:26:09 -0700

    Add to generated makefiles full pack selection

--- projects/bitos-windows-default-bit.h
+++ projects/bitos-windows-default-bit.h
@@ -131,7 +131,7 @@
 
 /* Profile */
 #ifndef BIT_CONFIG_CMD
-    #define BIT_CONFIG_CMD "bit -d -q -platform windows-x86-default -configure . --without default -gen vs"
+    #define BIT_CONFIG_CMD "bit -d -q -platform windows-x86-default -configure . --without default -gen nmake"
 #endif
 #ifndef BIT_BITOS_PRODUCT
     #define BIT_BITOS_PRODUCT 1

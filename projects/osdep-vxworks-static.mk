a57fa69c719f8add97b246eda718dc3bdee9232b
Embedthis Software <dev@embedthis.com>
2014-03-15 21:07:18 -0700

    REFACTOR: Rename bitos => osdep

---
+++ projects/osdep-vxworks-static.mk
@@ -0,0 +1,139 @@
+#
+#   osdep-vxworks-static.mk -- Makefile to build Embedthis O/S Dependent Layer for vxworks
+#
+
+NAME                  := osdep
+VERSION               := 0.8.0
+PROFILE               ?= static
+ARCH                  ?= $(shell echo $(WIND_HOST_TYPE) | sed 's/-.*//')
+CPU                   ?= $(subst X86,PENTIUM,$(shell echo $(ARCH) | tr a-z A-Z))
+OS                    ?= vxworks
+CC                    ?= cc$(subst x86,pentium,$(ARCH))
+LD                    ?= link
+CONFIG                ?= $(OS)-$(ARCH)-$(PROFILE)
+LBIN                  ?= $(CONFIG)/bin
+PATH                  := $(LBIN):$(PATH)
+
+
+ME_EXT_COMPILER_PATH  ?= cc$(subst x86,pentium,$(ARCH))
+ME_EXT_LIB_PATH       ?= ar
+ME_EXT_LINK_PATH      ?= link
+ME_EXT_VXWORKS_PATH   ?= $(WIND_BASE)
+ME_EXT_WINSDK_PATH    ?= winsdk
+
+export WIND_HOME      ?= $(WIND_BASE)/..
+export PATH           := $(WIND_GNU_PATH)/$(WIND_HOST_TYPE)/bin:$(PATH)
+
+CFLAGS                += -fno-builtin -fno-defer-pop -fvolatile -w
+DFLAGS                += -DVXWORKS -DRW_MULTI_THREAD -D_GNU_TOOL -DCPU=PENTIUM $(patsubst %,-D%,$(filter ME_%,$(MAKEFLAGS))) 
+IFLAGS                += "-I$(CONFIG)/inc -I$(WIND_BASE)/target/h -I$(WIND_BASE)/target/h/wrn/coreip"
+LDFLAGS               += '-Wl,-r'
+LIBPATHS              += -L$(CONFIG)/bin
+LIBS                  += -lgcc
+
+DEBUG                 ?= debug
+CFLAGS-debug          ?= -g
+DFLAGS-debug          ?= -DME_DEBUG
+LDFLAGS-debug         ?= -g
+DFLAGS-release        ?= 
+CFLAGS-release        ?= -O2
+LDFLAGS-release       ?= 
+CFLAGS                += $(CFLAGS-$(DEBUG))
+DFLAGS                += $(DFLAGS-$(DEBUG))
+LDFLAGS               += $(LDFLAGS-$(DEBUG))
+
+ME_ROOT_PREFIX        ?= deploy
+ME_BASE_PREFIX        ?= $(ME_ROOT_PREFIX)
+ME_DATA_PREFIX        ?= $(ME_VAPP_PREFIX)
+ME_STATE_PREFIX       ?= $(ME_VAPP_PREFIX)
+ME_BIN_PREFIX         ?= $(ME_VAPP_PREFIX)
+ME_INC_PREFIX         ?= $(ME_VAPP_PREFIX)/inc
+ME_LIB_PREFIX         ?= $(ME_VAPP_PREFIX)
+ME_MAN_PREFIX         ?= $(ME_VAPP_PREFIX)
+ME_SBIN_PREFIX        ?= $(ME_VAPP_PREFIX)
+ME_ETC_PREFIX         ?= $(ME_VAPP_PREFIX)
+ME_WEB_PREFIX         ?= $(ME_VAPP_PREFIX)/web
+ME_LOG_PREFIX         ?= $(ME_VAPP_PREFIX)
+ME_SPOOL_PREFIX       ?= $(ME_VAPP_PREFIX)
+ME_CACHE_PREFIX       ?= $(ME_VAPP_PREFIX)
+ME_APP_PREFIX         ?= $(ME_BASE_PREFIX)
+ME_VAPP_PREFIX        ?= $(ME_APP_PREFIX)
+ME_SRC_PREFIX         ?= $(ME_ROOT_PREFIX)/usr/src/$(NAME)-$(VERSION)
+
+
+
+unexport CDPATH
+
+ifndef SHOW
+.SILENT:
+endif
+
+all build compile: prep $(TARGETS)
+
+.PHONY: prep
+
+prep:
+	@echo "      [Info] Use "make SHOW=1" to trace executed commands."
+	@if [ "$(CONFIG)" = "" ] ; then echo WARNING: CONFIG not set ; exit 255 ; fi
+	@if [ "$(ME_APP_PREFIX)" = "" ] ; then echo WARNING: ME_APP_PREFIX not set ; exit 255 ; fi
+	@if [ "$(WIND_BASE)" = "" ] ; then echo WARNING: WIND_BASE not set. Run wrenv.sh. ; exit 255 ; fi
+	@if [ "$(WIND_HOST_TYPE)" = "" ] ; then echo WARNING: WIND_HOST_TYPE not set. Run wrenv.sh. ; exit 255 ; fi
+	@if [ "$(WIND_GNU_PATH)" = "" ] ; then echo WARNING: WIND_GNU_PATH not set. Run wrenv.sh. ; exit 255 ; fi
+	@[ ! -x $(CONFIG)/bin ] && mkdir -p $(CONFIG)/bin; true
+	@[ ! -x $(CONFIG)/inc ] && mkdir -p $(CONFIG)/inc; true
+	@[ ! -x $(CONFIG)/obj ] && mkdir -p $(CONFIG)/obj; true
+	@[ ! -f $(CONFIG)/inc/me.h ] && cp projects/osdep-vxworks-static-me.h $(CONFIG)/inc/me.h ; true
+	@if ! diff $(CONFIG)/inc/me.h projects/osdep-vxworks-static-me.h >/dev/null ; then\
+		cp projects/osdep-vxworks-static-me.h $(CONFIG)/inc/me.h  ; \
+	fi; true
+	@if [ -f "$(CONFIG)/.makeflags" ] ; then \
+		if [ "$(MAKEFLAGS)" != " ` cat $(CONFIG)/.makeflags`" ] ; then \
+			echo "   [Warning] Make flags have changed since the last build: "`cat $(CONFIG)/.makeflags`"" ; \
+		fi ; \
+	fi
+	@echo $(MAKEFLAGS) >$(CONFIG)/.makeflags
+
+clean:
+
+clobber: clean
+	rm -fr ./$(CONFIG)
+
+
+
+#
+#   version
+#
+version: $(DEPS_1)
+	echo 0.8.0
+
+#
+#   stop
+#
+stop: $(DEPS_2)
+
+#
+#   installBinary
+#
+installBinary: $(DEPS_3)
+
+#
+#   start
+#
+start: $(DEPS_4)
+
+#
+#   install
+#
+DEPS_5 += stop
+DEPS_5 += installBinary
+DEPS_5 += start
+
+install: $(DEPS_5)
+
+#
+#   uninstall
+#
+DEPS_6 += stop
+
+uninstall: $(DEPS_6)
+
